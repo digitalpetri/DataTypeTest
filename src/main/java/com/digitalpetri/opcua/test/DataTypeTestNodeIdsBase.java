e2360ffe3bfbb80999e26c4ed575ca80085b51a1
Kevin Herron <kevinherron@gmail.com>
2025-04-09 05:55:16 -0700

    Initial commit

---
+++ src/main/java/com/digitalpetri/opcua/test/DataTypeTestNodeIdsBase.java
@@ -0,0 +1,6 @@
+package com.digitalpetri.opcua.test;
+
+abstract class DataTypeTestNodeIdsBase {
+
+  protected static final String NAMESPACE_URI = "https://github.com/digitalpetri/DataTypeTest";
+}
